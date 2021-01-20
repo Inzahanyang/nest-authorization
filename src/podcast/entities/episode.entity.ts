463bde8ba97deda8d1de59eff2a0664f0f86aea0
inzahan <inzahanyang@gmail.com>
2021-01-20 23:24:31 +0900

    nest authorization

---
+++ src/podcast/entities/episode.entity.ts
@@ -0,0 +1,26 @@
+import { ObjectType, Field, InputType } from "@nestjs/graphql";
+import { IsString } from "class-validator";
+import { Column, Entity, ManyToOne, RelationId } from "typeorm";
+import { CoreEntity } from "./core.entity";
+import { Podcast } from "./podcast.entity";
+
+@InputType("EpisodeInputType", { isAbstract: true })
+@Entity()
+@ObjectType()
+export class Episode extends CoreEntity {
+  @Column()
+  @Field(type => String)
+  @IsString()
+  title: string;
+
+  @Column()
+  @Field(type => String)
+  @IsString()
+  category: string;
+
+  @Field(type => Podcast)
+  @ManyToOne(() => Podcast, podcast => podcast.episodes, {
+    onDelete: "CASCADE",
+  })
+  podcast: Podcast;
+}
